1f2944a48f06cb4bc42dfa6b75e29c568f2d20f8
Wallace0 <nigelxwalters@gmail.com>
2025-03-16 10:29:12 +0000

    Update sc_flags.lua - add some init debug chat messages

--- sc_flags.lua
+++ sc_flags.lua
@@ -254,6 +254,13 @@ local function initializeSCFlagScript()
     debug = false
     getStates()
     reInitailizeVars()
+    if (driverCar ==nil) then
+        ac.sendChatMessage("SC: INFO | SC Flags Script Initialisation ERROR driverCar not identified!")
+    elseif (safetyCar == nil) then
+        ac.sendChatMessage("SC: INFO | SC Flags Script Initialisation ERROR safetyCar not identified for driver " .. driverCar:driverName())
+    else
+        ac.sendChatMessage("SC: INFO | SC Flags Script Initialisation " .. driverCar:driverName() .. " - SC - " .. safetyCar:driverName())
+    end
 end
 
 -- Define the callback function
