413a3fd45f156173187d5c8bad542d629a86a26a
Wallace0 <nigelxwalters@gmail.com>
2025-03-16 10:36:35 +0000

    Update sc_flags.lua - more logging

--- sc_flags.lua
+++ sc_flags.lua
@@ -246,6 +246,7 @@ local function repositionFlags()
 end
 
 local function initializeSCFlagScript()
+    writeLog("SC: Initialization called.")
     flagColor = rgbm.colors.gray
     showFlags = false
     goGreen = false
@@ -254,6 +255,7 @@ local function initializeSCFlagScript()
     debug = false
     getStates()
     reInitailizeVars()
+    writeLog("SC: Initialization finished - sending notification messages.")
     if (driverCar ==nil) then
         ac.sendChatMessage("SC: INFO | SC Flags Script Initialisation ERROR driverCar not identified!")
     elseif (safetyCar == nil) then
