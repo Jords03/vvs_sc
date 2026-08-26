6309a870d8afca81d46e208240527a7d5bb18f40
Wallace0 <nigelxwalters@gmail.com>
2026-08-26 09:22:30 +0100

    Added Rich as admin

--- sc_flags.lua
+++ sc_flags.lua
@@ -1,7 +1,7 @@
 SCRIPT_NAME = "VVS Safety Car Flags Mark2"
 SCRIPT_SHORT_NAME = "VVSSCFLAGS2"
-SCRIPT_VERSION = "0.0.1.17"
-SCRIPT_VERSION_CODE = 00017
+SCRIPT_VERSION = "0.0.1.18"
+SCRIPT_VERSION_CODE = 00018
 
 --####################################################################################################
 --####################################### GLOBALS ####################################################
@@ -19,7 +19,7 @@ local ovalTrackIDs = {
 
 
 --CONSTANTS
-local adminCarNames = {"Jon Astrop", "Dominic Fovargue", "Nigel Walters"}
+local adminCarNames = {"Jon Astrop", "Dominic Fovargue", "Nigel Walters", "Rich Snape"}
 local safetyCarName = "Safety Car"
 --distance you should be within when following
 local distanceThreshold = 16
